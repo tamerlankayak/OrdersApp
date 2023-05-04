0594512dc3c9d7f35f521a7c1a3fe63a20f44784
tamerlankayak <tamerlan_94@mail.ru>
2023-05-04 14:36:39 +0400

    dummy data

--- app/src/main/java/com/example/ordersapp/deliverer_feature/data/mapper/DelivererMapper.kt
+++ app/src/main/java/com/example/ordersapp/deliverer_feature/data/mapper/DelivererMapper.kt
@@ -3,6 +3,7 @@ package com.example.ordersapp.deliverer_feature.data.mapper
 import com.example.ordersapp.core.data.local.entities.DelivererEntity
 import com.example.ordersapp.core.domain.model.Deliverer
 
+//clean architecture for mapping objects
 fun Deliverer.toDelivererEntity(): DelivererEntity {
     return DelivererEntity(
         delivererId = delivererId,
