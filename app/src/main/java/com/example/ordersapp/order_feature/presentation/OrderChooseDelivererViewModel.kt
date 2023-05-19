d23d5bf59de696a74f895e5b09e976a1a0b0e31f
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-05-19 17:38:42 +0400

    Update OrderChooseDelivererViewModel.kt

--- app/src/main/java/com/example/ordersapp/order_feature/presentation/OrderChooseDelivererViewModel.kt
+++ app/src/main/java/com/example/ordersapp/order_feature/presentation/OrderChooseDelivererViewModel.kt
@@ -15,6 +15,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+//injection of classes
 @HiltViewModel
 class OrderChooseDelivererViewModel @Inject constructor(
     private val orderRepository: OrderRepository,
